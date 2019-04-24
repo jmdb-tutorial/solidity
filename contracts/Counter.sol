b16e81956dfe0bf2edacb10d029e2834b37a64c0
Jim Barritt <jimb@thoughtworks.com>
2019-04-24 10:47:43 +0100

    First commit

---
+++ contracts/Counter.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+contract Counter {
+    int private count = 0;
+    function incrementCounter() public {
+        count += 1;
+    }
+    function decrementCounter() public {
+        count -= 1;
+    }
+    function getCount() public view returns (int) {
+        return count;
+    }
+}
